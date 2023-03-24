fba9d3293da8af2ecf482c8b9c59fc16003df298
fenix144 <yan_1731@hotmail.com>
2023-03-24 13:31:25 -0400

    Add Parser and Config class

--- Src/Parser.cpp
+++ Src/Parser.cpp
@@ -5,7 +5,6 @@ Parser::Parser() {}
 Parser::Parser(char **argv): arg(argv[1]) { }
 
 void Parser::parse_config_file(void) {
-
+	// server {info} server marks the type of block and the brackets mark the start and end of the server block
+	
 }
-
-// server {info} server marks the type of block and the brackets mark the start and end of the server block
