62614d9693c83ab18063a2a95d80c8d70af3bb9e
xApologize <jenbenrossignol@hotmail.com>
2023-04-03 14:31:56 -0400

    done exploring gtest

--- Doc/learn/unit-test_test/basic.cpp
+++ Doc/learn/unit-test_test/basic.cpp
@@ -1,4 +1,18 @@
+#include <gtest/gtest.h>
+
 int calculator(int x, int y)
 {
 	return (x + y);
+}
+
+TEST(basic, calculator)
+{
+	EXPECT_EQ(calculator(2,2), 4);
+	EXPECT_EQ(calculator(2,2), 4);
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
