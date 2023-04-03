c297d8a91710d862ebb211693914ec7f51eec9b9
fenix144 <yan_1731@hotmail.com>
2023-04-03 16:34:25 -0400

    Still needs work

--- Sources/Parser.class.cpp
+++ Sources/Parser.class.cpp
@@ -25,36 +25,81 @@ void Parser::parse_config_file(void) {
 	}	
 }
 
-void Parser::findAmountServers(void) { //trying a different approach
+void Parser::readfile(void) {
+	this->openFile();
 	string buffer;
+	while (std::getline(this->config_file, buffer, '\n')){
+		cout << buffer << endl;
+	}
+}
+
+void Parser::findAmountServers(void) { //can prolly do better
+	string buffer;
+	string comp("{ 	");
 	bool checkServer = false;
 	int lock = 0;
 	this->openFile();
 	while (std::getline(this->config_file, buffer, '\n')) {
-		while (buffer.find("{") == string::npos) {
-			if (buffer.find("}") != string::npos && lock == 0)
+		if (buffer.find("server") != string::npos){
+			if (comp.find(buffer[buffer.find("server") + 6]) == string::npos && buffer[buffer.find("server") + 6] != '\0')
 				throw InvalidConfigFile();
-			if (!std::getline(this->config_file, buffer, '\n'))
-				throw InvalidConfigFile();
+			checkServer = true;
 		}
-		if (buffer.find("}") == string::npos)
-			lock++;
-		if (buffer.find("{") > buffer.find("}"))
-				throw InvalidConfigFile();
-		while (lock != 0) {
-			if (!std::getline(this->config_file, buffer, '\n'))
-				throw InvalidConfigFile();
-			if (buffer.find("}") != string::npos)
-				lock--;
-			if (buffer.find("{") != string::npos)
+		while (checkServer == true) {
+			cout << buffer << endl;
+			if (buffer.find('{') != string::npos && buffer.find('}') != string::npos) {
+				if (buffer.find('{') > buffer.find('}')){
+					throw InvalidConfigFile();   //Throwing exception if closing curly bracket comes before opening curly bracket
+				}
+			}
+			if (buffer.find('{') != string::npos) {
+				cout << "lock ++" << endl;
 				lock++;
+			}
+			if (buffer.find('}') != string::npos) {
+				cout << "lock --" << endl;
+				lock--;
+			}
+			if (lock == 0) {
+				NServ++;
+				checkServer = false;
+			}
+			if (checkServer == true)
+				std::getline(this->config_file, buffer, '\n');
 		}
-		if (lock!= 0)
-			throw InvalidConfigFile();
-		else
-			NServ++;
 	}
 }
+
+// void Parser::findAmountServers(void) { //trying a different approach
+// 	string buffer;
+// 	bool checkServer = false;
+// 	int lock = 0;
+// 	this->openFile();
+// 	while (std::getline(this->config_file, buffer, '\n')) {
+// 		while (buffer.find("{") == string::npos) {
+// 			if (buffer.find("}") != string::npos && lock == 0)
+// 				throw InvalidConfigFile();
+// 			if (!std::getline(this->config_file, buffer, '\n'))
+// 				throw InvalidConfigFile();
+// 		}
+// 		if (buffer.find("}") == string::npos)
+// 			lock++;
+// 		if (buffer.find("{") > buffer.find("}"))
+// 				throw InvalidConfigFile();
+// 		while (lock != 0) {
+// 			if (!std::getline(this->config_file, buffer, '\n'))
+// 				throw InvalidConfigFile();
+// 			if (buffer.find("}") != string::npos)
+// 				lock--;
+// 			if (buffer.find("{") != string::npos)
+// 				lock++;
+// 		}
+// 		if (lock!= 0)
+// 			throw InvalidConfigFile();
+// 		else
+// 			NServ++;
+// 	}
+// }
 
 unsigned int Parser::getNServ(void) const {
 	return NServ;
