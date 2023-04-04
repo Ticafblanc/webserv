8e735a9c4cd3b13b2d110220d89377fe675355ae
mdoquocb <mdoquocb@student.42quebec.com>
2023-04-04 14:53:37 -0400

    add poll to wait client in the place of accept refactor directory like ngnix add 2 website temporary prepare 2 files to conf remove old website

--- usr->local/var/www/webserv1.com/js/console.js
+++ usr->local/var/www/webserv1.com/js/console.js
@@ -1,40 +1,35 @@
-let page = {};
-page.console = (function(){
-    let console = document.querySelector('#console .main');
+// import * as readline from 'node:readline';
+// import * as readline from 'node:readline/promises';
+// import * as readline from 'node:readline/promises';
+// import { stdin as input, stdout as output } from 'node:process';
 
-    return {
-        // print: function(text){
-        //     text = (text !== undefined ? text : '');
-        //     text = String(text);
-        //     text = text.replace(/ /g, '\u00A0');
-        //     setTimeout(() => {
-        //         console.innerText += text
-        //         console.scrollTop = console.scrollHeight - console.clientHeight;
-        //     });
-        // },
-        //
-        // println: function(text){
-        //     text = (text !== undefined ? text : '');
-        //     text = String(text);
-        //     text = text.replace(/ /g, '\u00A0');
-        //     setTimeout(() => {
-        //         console.innerText += text + '\n'
-        //         console.scrollTop = console.scrollHeight - console.clientHeight;
-        //     });
-        // },
-        //
-        // clear: function(){
-        //     setTimeout(() => {
-        //         console.innerHTML = ''
-        //         console.scrollTop = console.scrollHeight - console.clientHeight;
-        //     });
-        // },
-        //
-        // input: function(text){
-        //     this.print(text);
-        //     value = prompt(text);
-        //     this.println(value);
-        //     return value;
-        // }
-    }
-})();
+const readline = require('readline');
+const { spawn } = require('child_process');
+
+// const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Webserv > ', (command) => {
+    const child = spawn('bash', ['-c', command]);
+    child.stdout.on('data', (data) => {
+        page.console.print(`Webserv > ${data}\n`);
+    });
+    // child.stderr.on('data', (data) => {
+    //     write.innerText += `Webserv > ${data}\n`;
+    // });
+    // child.on('close', (code) => {
+    //     write.innerText += `Process finished with exit code ${code}\n`;
+    //     rl.close();
+    // });
+    // rl.on('close', () => {
+    //     write.innerText += `Interface de ligne de commande fermée\n`;
+    //     while (true) {
+    //         // Attendre une entrée utilisateur pour fermer le programme
+    //     }
+    // });
+});
+
+
