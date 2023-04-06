3d3a00fa46ce12b44f1d75971b2b1362902725f7
jeanbenoit.rossignol.eleve <jeanbenoit.rossignol.eleve@gmail.com>
2023-04-06 15:06:58 -0400

    change how the tests work completely. add test in the cmake and move function outside of main for better readability

---
+++ Unit_test/Sources/unitTestLauncher.cpp
@@ -0,0 +1,22 @@
+#include "../Include/unit_test.hpp"
+
+int unitTestLauncher()
+{
+	try {
+		std::vector<server> server_vec = init("../../usr->local/etc/webserv/webserv.conf");
+		std::vector<server>::iterator server_it = server_vec.begin();
+		for(; server_it != server_vec.end(); server_it++)
+			server_it->launcher();
+	}
+	catch (const std::exception & e) {
+		e.what();
+		return (TEST_FAIL);
+	}
+	return (TEST_PASS);
+}
+
+int main ()
+{
+	TEST_EQ(unitTestLauncher(), TEST_PASS);
+	return (TEST_PASS);
+}
