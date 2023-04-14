ae30108c0c3a46bcdd7f61ed680cbea1355e6791
fenix144 <yan_1731@hotmail.com>
2023-04-14 16:44:09 -0400

    Add access methods

--- Sources/Route.class.cpp
+++ Sources/Route.class.cpp
@@ -3,3 +3,51 @@
 Route::Route() {}
 
 Route::~Route() {}
+
+void Route::setMatch(string& match) throw() {
+    this->_match = match;
+}
+
+string& Route::getMatch(void) throw() {
+    return this->_match;
+}
+
+void Route::setHttpRedir(string& redir) throw() {
+    this->_httpRedir = redir;
+} 
+
+string& Route::getHttpRedir(void) throw() {
+    return this->_httpRedir;
+}
+
+void Route::setDirectoryListing(bool state) throw() {
+    this->_directoryListing = state;
+}
+
+bool Route::getDirectoryListing(void) throw() {
+    return this->_directoryListing;
+}
+
+void Route::setSearchDir(string& dir) throw() {
+    this->_searchDir = dir;
+}
+
+string& Route::getSearchDir(void) throw() {
+    return this->_searchDir;
+}
+
+void Route::setDefaultDirFile(string& dirFile) throw() {
+    this->_defaultDirFile = dirFile;
+}
+
+string& Route::getDefaultDirFile(void) throw() {
+    return this->_defaultDirFile;
+}
+
+void Route::setSavePath(string& path) throw() {
+    this->_savePath = path;
+}
+
+string& Route::getSavePath(void) throw() {
+    return this->_savePath;
+}
