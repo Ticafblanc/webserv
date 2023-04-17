28610ffc5f78f54a39c218f34106038d10b57da3
ticafblanc <matthisdqb@icloud.com>
2023-04-17 14:41:38 -0400

    test docker

--- set_docker/Build_Container.sh
+++ set_docker/Build_Container.sh
@@ -12,5 +12,37 @@
 
 #!/bin/bash
 
-docker build -t webserv_container .
+echo "write a represetory you want to load in your container"
+read REPO
+if [ -z "${REPO}"] ; then
+  REPO="https://github.com/Ticafblanc/webserv.git"
+fi
 
+echo "name of container"
+read NAME
+if [ -z "${NAME}"] ; then
+  NAME="webserv"
+fi
+
+echo "path of work directory"
+read WORKDIR
+if [ -z "${WORKDIR}"] ; then
+  WORKDIR=$PWD
+fi
+
+cd "${WORKDIR}"
+
+if docker images | grep -q ubuntu:latest ; then
+  echo "Image already pull"
+else
+  echo "load image ubuntu:latest"
+  docker pull ubuntu:latest
+fi
+
+docker build -t webserv:latest -f /webserv/set_docker/Dockerfile ${REPO}
+
+
+#docker build -t clion/remote-cpp-env:0.5 .
+
+alias dr='docker run --name ${NAME} -v ${WORKDIR}:/webserv_container/webserv webserv:latest'
+alias webserv='docker exec -it ${NAME} /bin/bash'
