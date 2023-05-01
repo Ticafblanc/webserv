0228a4216e7d01cd3ac05aa03c9367e7ea863f1b
yann <yan_1731@hotmail.com>
2023-05-01 16:54:40 -0400

    Modify test to work + move initialization of iData into init

--- Unit_test/Sources/unitTestInit.cpp
+++ Unit_test/Sources/unitTestInit.cpp
@@ -21,7 +21,6 @@ int unitTestInit()
         cerr << e.what() << endl;
         return (TEST_FAIL);
     }
-    cerr << "Success lol?" << endl;
     return (TEST_PASS);
 }
 
@@ -39,24 +38,18 @@ int unitTestGetter()
             TEST_EQ(vecip[0].first, ip);
             TEST_EQ(vecip[0].second, port);
             TEST_EQ(server_it->getDataServer().getDomain(), AF_INET);
-            cerr << "let's go" << endl;
             TEST_EQ(server_it->getDataServer().getType(), SOCK_STREAM);
             TEST_EQ(server_it->getDataServer().getProtocol(), 0);
             TEST_EQ(server_it->getDataServer().getBacklog(), 10);
-            TEST_EQ(server_it->getDataServer().getAddress().sin_family, server_it->getDataServer().getDomain());
-            TEST_EQ(server_it->getDataServer().getAddress().sin_addr.s_addr, inet_addr(ip.c_str()));
-            TEST_EQ(server_it->getDataServer().getAddress().sin_port, htons(port));
             TEST_EQ(server_it->getDataServer().getLevel(), SOL_SOCKET);
             TEST_EQ(server_it->getDataServer().getOptionName(), SO_REUSEADDR);
             TEST_EQ(server_it->getDataServer().getOptionVal(), 1);
 		}
     }
     catch (const std::exception & e) {
-        cerr << "Fail lol" << endl;
         cerr << e.what() << endl;
         return (TEST_FAIL);
     }
-    cerr << "Success again lol?" << endl;
     return (TEST_PASS);
 }
 
