f082bbea41b1b0908c3d2250d5a171f155a62b69
Matthis Doquocbao <mdoquocb@c3r1p5.42quebec.com>
2023-05-10 11:15:42 -0400

    include config_server done continue on cpp

--- Include/config_server.class.hpp
+++ Include/config_server.class.hpp
@@ -1,13 +1,14 @@
-//
-// Created by Matthis Doquocbao on 5/10/23.
-//
+
 
 #ifndef WEBSERVER_CONFIG_SERVER_CLASS_HPP
 #define WEBSERVER_CONFIG_SERVER_CLASS_HPP
 
+#include <Include/header.hpp>
 #include <Include/config_address.class.hpp>
+#include <Include/config_webserv.class.hpp>
 
 class config_server {
+
 /*>********************************public section**********************************/
 
 public:
@@ -60,6 +61,82 @@ public:
 
 /*
 *====================================================================================
+*|                                  Member Exception                                 |
+*====================================================================================
+*/
+
+/**
+ * Class exception of config_server class
+ *
+ * class config_exception;
+ *
+ * @inherit std::exception
+ **/
+    class config_exception: public std::exception
+    {
+    public:
+
+        /**
+         * Constructor of config_exception class
+         *
+         * config_exception(config_server & config, const char* message);
+         *
+         * @param   config_server is a config_server reference to set the private _config
+         *          to manage the close of config_server class
+         *          message to store const char*
+         * @throw   none
+         **/
+        config_exception(config_server&, const char *);
+
+        /**
+         * Copy constructor of config_exception class
+         *
+         * config_exception(config_exception &);
+         *
+         * @param   config_exception instance to build the config_exception
+         * @throw   none
+         **/
+        config_exception(const config_exception &);
+
+        /**
+         * Operator overload= of config_exception class
+         *
+         * operator=(const config_exception&);
+         *
+         * @param   config_exception instance const to copy the config_exception
+         * @throw   none
+         **/
+        config_exception& operator=(const config_exception &);
+
+        /**
+        * Destructor of config_exception class
+        *
+        * virtual ~config_exception() throw();
+        *
+        * @throw   none
+        **/
+        virtual ~config_exception() throw();
+
+        /**
+         * Public methode of config_exception
+         *
+         * virtual const char * what() const throw();
+         *
+         * @returns  const char * store in private std::string _message
+         *          at the construction defaut constructor "socket error"
+         * @param   void
+         * @throw   none
+         **/
+        virtual const char * what() const throw();
+
+    private:
+        std::string     _message;
+        config_server & _config;
+    };
+
+
+/*
+*====================================================================================
 *|                                  Element access                                 |
 *====================================================================================
 */
@@ -67,75 +144,72 @@ public:
 /**
  * Public methode of config_server.class class
  *
- * std::string& get_server_name(int position);
+ * std::vector<std::string> &get_vector_server_name() const;
  *
- * @returns     string& contain name of server
- * @param       position int is a position of variable in vector<std::string> table,
- *              represente the one of name of server
- * @throw       data_exception out of rang
+ * @returns     std::vector<std::string>& contain all names of server
+ * @param       void
+ * @throw       none
  */
-    std::string &get_server_name(int);
+    std::vector<std::string> &get_vector_server_name() const;
 
 
 /**
  * Public methode of config_server.class class
  *
- * void set_server_name(std::string & name);
+ * void set_vector_server_name(std::string & name);
  *
  * @returns     void
  * @param       name is an std::string contain name of server to add
- * @throw       data_exception if name already exist
+ * @throw       config_exception if name already exist
  */
-    void add_server_name(std::string &);
+    void add_vector_server_name(std::string &);
 
 /**
  * Public methode of config_server.class class
  *
- * std::string& get_ip_address(int position);
+ * std::string& get_ip_address();
  *
- * @returns     string& contain ip address of server
- * @param       position int is a position of variable in vector<std::string> table,
- *              represente the one of ip address of server
- * @throw       data_exception out of rang
+ * @returns     std::vector<std::string>& contain all names of server
+ * @param       void
+ * @throw       config_exception out of rang
  */
-    std::string &get_ip_address(int);
+    std::vector<std::string>  &get_vector_sock_address() const;
 
 
 /**
  * Public methode of config_server.class class
  *
- * void set_ip_address(std::string & name);
+ * void add_vector_sock_address(std::string ip_address, const int port);
  *
  * @returns     void
- * @param       name is an std::string contain ip address of server to add
- * @throw       data_exception if name already exist
+ * @param       ip_adress is a std::string with ip_address
+ *              port is an int with port to add
+ * @throw       config_exception if ip/port already exist in _webserv instance
  */
-    void add_ip_address(std::string &);
+    void add_vector_sock_address(std::string, const int );
 
 /**
  * Public methode of config_server.class class
  *
- * std::string& get_ip_address(int position);
+ * int &get_backlog();
  *
- * @returns     struct sockaddr& contain struct sockaddr of server
- * @param       position int is a position of variable in vector<struct sockaddr> table,
- *              represente the one of struct sockaddr of server
- * @throw       data_exception out of rang
+ * @returns     int with the maximum number of pending connections
+ * @param       void
+ * @throw       none
  */
-    struct sockaddr &get_sock_address(int);
+    int &get_backlog();
 
 
 /**
  * Public methode of config_server.class class
  *
- * void set_ip_address(std::string & ip_address);
+ * void set_backlog(unsign int backlog);
  *
  * @returns     void
- * @param       ip_address is an std::string contain ip address of server to
- *              build struct sockaddr and add to vector
- * @throw       data_exception if name already exist
+ * @param       backlog is an int contain the maximum number of pending connections
+ * @throw       config_exception if
  */
-    void add_sock_address(std::string &);
+    void set_backlog(unsigned int);
 
 /*>*******************************private section**********************************/
 
@@ -146,9 +220,10 @@ private:
 *|                                     Member                                       |
 *====================================================================================
 */
-    std::vector<config_address>       _vector_sock_address;
     std::vector<std::string>        _vector_server_name;
-    int                             _backlog;//SOMAXCONN
+    std::vector<config_address>     _vector_sock_address;
+    unsigned int                    _backlog;//SOMAXCONN
+    const config_webserv &          _webserv;
 
 };
 
--- Sources/config_server.class.cpp
+++ Sources/config_server.class.cpp
@@ -1,5 +1,3 @@
-//
-// Created by Matthis Doquocbao on 5/10/23.
-//
+
 
 #include "Include/config_server.class.hpp"
