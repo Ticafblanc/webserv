b72d1a4b2fc75ec5feda1b7bf019caec45ac46f8
xApologize <jenbenrossignol@hotmail.com>
2023-07-15 14:12:42 -0400

    add siteweb for matthis

---
+++ site_web/src/pages/FormPage.tsx
@@ -0,0 +1,12 @@
+import FormComponent from '../components/FormComponent.tsx'
+import '../css/App.css'
+
+function FormPage() {
+    return (
+        <div className="centered">
+            <FormComponent/>
+        </div>
+    )
+}
+
+export default FormPage;
