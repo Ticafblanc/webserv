352a8f90a0c9be3d40c89da6016d506536e8560e
xApologize <jenbenrossignol@hotmail.com>
2023-08-22 14:50:27 -0400

    move basic test to conversion main for test purpose

---
+++ Source_Code/4.1-ConversionUtils/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ben on 2023-08-21.
+//
+#include <iostream>
+#include "Includes/ConversionUtils.hpp"
+
+int main() {
+    std::string test;
+    test = intToString(42);
+    std::cout << test << std::endl;
+    return (0);
+}
