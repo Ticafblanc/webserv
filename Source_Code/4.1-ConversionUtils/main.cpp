92fc23ede9ba20f85b69470c7c479214c1a3f136
xApologize <jenbenrossignol@hotmail.com>
2023-08-22 15:27:41 -0400

    add new test in main

--- Source_Code/4.1-ConversionUtils/main.cpp
+++ Source_Code/4.1-ConversionUtils/main.cpp
@@ -5,8 +5,16 @@
 #include "Includes/ConversionUtils.hpp"
 
 int main() {
-    std::string test;
-    test = intToString(42);
-    std::cout << test << std::endl;
+    //basic test for int to string, i need to add more later
+    std::string string_test;
+    string_test = intToString(42);
+    std::cout << "intToString test" << std::endl;
+    std::cout << string_test << '\n' << std::endl;
+
+    //basic test for string to sizet, i need to add more later
+    size_t size_t_test;
+    size_t_test = hexaStringToSizet("42");
+    std::cout << "stringToSizeT test" << std::endl;
+    std::cout << size_t_test << std::endl;
     return (0);
 }
