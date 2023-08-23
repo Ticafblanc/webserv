f77ee08907b3f1b9c29d560a960ede7d0f16e14f
xApologize <jenbenrossignol@hotmail.com>
2023-08-23 14:48:26 -0400

    change to a static syntax

--- Source_Code/4.1-Converter/main.cpp
+++ Source_Code/4.1-Converter/main.cpp
@@ -8,19 +8,19 @@ int main() {
     Converter Converter;
     //basic test for int to string, i need to add more later
     std::string string_test;
-    string_test = Converter.intToString(42);
+    string_test = Converter::intToString(42);
     std::cout << "intToString test" << std::endl;
     std::cout << string_test << '\n' << std::endl;
 
     //basic test for hex string to sizet, i need to add more later
     size_t size_t_test;
-    size_t_test = Converter.hexStringToSizet("42");
+    size_t_test = Converter::hexStringToSizet("42");
     std::cout << "hexStringToSizeT test" << std::endl;
     std::cout << size_t_test << '\n' << std::endl;
 
     //basic test for size_t to hex string, i need to add more later
     std::string hex_string_test;
-    hex_string_test = Converter.sizetToHexString(42);
+    hex_string_test = Converter::sizetToHexString(42);
     std::cout << "sizetToStringHex test" << std::endl;
     std::cout << hex_string_test << std::endl;
 
