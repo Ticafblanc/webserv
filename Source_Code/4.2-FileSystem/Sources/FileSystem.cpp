8f2f93b608eb4c4a9693b38520212c5552dc0bb3
xApologize <jenbenrossignol@hotmail.com>
2023-08-25 15:38:04 -0400

    add empty array to please clion

--- Source_Code/4.2-FileSystem/Sources/FileSystem.cpp
+++ Source_Code/4.2-FileSystem/Sources/FileSystem.cpp
@@ -4,7 +4,7 @@
 #include <Source_Code/4.2-FileSystem/Includes/FileSystem.hpp>
 
 bool isFile(const std::string & path) {
-    struct stat statbuf;
+    struct stat statbuf = {};
     if (stat(path.c_str(), &statbuf) != 0) {
         return false;
     }
