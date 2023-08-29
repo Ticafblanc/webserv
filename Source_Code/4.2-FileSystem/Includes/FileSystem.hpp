1f5206a20ffbed121aa785b8cb896457e15c2d5f
xApologize <jenbenrossignol@hotmail.com>
2023-08-29 15:02:24 -0400

    add comment to class function

--- Source_Code/4.2-FileSystem/Includes/FileSystem.hpp
+++ Source_Code/4.2-FileSystem/Includes/FileSystem.hpp
@@ -19,13 +19,47 @@ public:
     FileSystem();
     ~FileSystem();
 
+    /**
+     * Check if the path passed in parameter is a file or not
+     *
+     * @param path of the file to check
+     * @return bool depending if the path is a file
+     */
     static bool isFile(const std::string & path);
+
+    /**
+     * Check if the path passed in parameter is an executable file or not
+     *
+     * @param path of the file to check
+     * @return bool depending if the path is an executable file
+     */
     static bool isExec(std::string & path);
+
+    /**
+     * Remove a file
+     *
+     * @param path of the file to remove
+     * @return bool if the file is removed correctly
+     */
     static bool removeFile(std::string & path);
 
+    /**
+     * Check if the path passed in parameter is a directory
+     *
+     * @param path of the directory to check
+     * @return bool is the path is a directory
+     */
     static bool isDirectory(const std::string & path);
+
+    /**
+     * Remove a directory
+     *
+     * @param path of the directory to remove
+     * @return bool is the directory is removed correctly
+     */
     static bool removeDirectory(std::string & path);
 
+    //will add comment later when i get to use it
     static bool extractFileToFd(const std::string & path, int fd, std::size_t & contentLength);
 };
 
