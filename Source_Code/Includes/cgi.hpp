2a91e55f05ca2cd195df6ba99d55492081baa05d
Matthis DoQuocBao <matthisdqb@icloud.com>
2024-02-22 12:56:58 -0500

    Update the server handling, configuration and HTTP header functionalities
    This commit modifies the functionality of HTTP header validation and parsing in the headers file. It enables the server file to accept client connections and process HTTP requests as well as replaces 'std::vector<std::string>' with the shorthand 'vecStr' in the configuration file. It also includes minor file renaming and removal of unnecessary files like 'socketManager.cpp'.

--- Source_Code/Includes/cgi.hpp
+++ Source_Code/Includes/cgi.hpp
@@ -4,7 +4,44 @@
 
 #ifndef WEBSERV_CGI_HPP
 #define WEBSERV_CGI_HPP
+#include "configuration.hpp"
+#include "headers.hpp"
+#include "main.hpp"
 
-class CGI {};
+class CGI {
+private:
+  std::string _cgi_path;
+  std::string _ressource_path;
+  Headers _request;
+  Configuration::server _conf;
+  Configuration::location _location;
+  std::map<std::string, std::string> _getParams(void);
+  std::string _getQueryString(void);
+  char **_convertParams(std::map<std::string, std::string> args);
+  void _freeArgs(char **args);
+  char *_newStr(std::string source);
+  std::string _execCGI(char **args);
+  char **_getExecArgs(void);
+  std::string _getScriptName(void);
+  std::string _removeQueryArgs(std::string query);
+
+public:
+  CGI(void);
+  CGI(std::string cgi_path, std::string ressource_path, HeadersBlock request,
+      Configuration::server conf, Configuration::location location);
+  ~CGI(void);
+  std::string getOutput(void);
+};
+
+class CGIException : public std::exception {
+private:
+  std::string _msg;
+
+public:
+  CGIException(std::string message = "Unable to execute CGI.")
+      : _msg(message){};
+  ~CGIException() throw(){};
+  const char *what() const throw() { return (_msg.c_str()); };
+};
 
 #endif // WEBSERV_CGI_HPP
