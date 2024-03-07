a5dd1ae5db5c6180d157c56f71e3934dd4b159c2
Matthis DoQuocBao <matthisdqb@icloud.com>
2024-03-07 11:10:11 -0500

    Update HTML and PHP layout, enhance CSS style, remove comments
    Revamped the layout structure of HTML and PHP pages to enhance webpage experience. Stylesheets have been improved for better visual appearance and user-friendly interaction. Unused form code comments have been removed, and current time feature in PHP has been uncommented to display the real-time update.

--- www/webserv.com/cgi-pass/get.php
+++ www/webserv.com/cgi-pass/get.php
@@ -5,25 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webserv 42</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 <header>
     <h1><a href="../accueille.html">Werbserv 42</a></h1>
+    <div class="test">
+        <a href="../html/index.html" class="static out">Static request</a>
+        <a href="index.php" class="cgi in">Dynamic request</a>
+    </div>
+    <aside>
+        <a href="get.php" class="cgi request in">GET request</a>
+        <a href="post.php" class="cgi request out">POST request</a>
+        <a href="delete.php" class="cgi request out">DELETE request</a>
+    </aside>
 </header>
 
-<div class="index">
-    <aside>
-        <a href="../accueille.html" class="aside">GET request</a>
-        <a href="../accueille.html" class="aside">POST request</a>
-        <a href="../accueille.html" class="aside">DELETE request</a>
-    </aside>
-    <main>
-        <img src="../image/static-site.png" alt="Static site" class="site">
-    </main>
-</div>
+<main>
+      <p>Date et Heure actuelle : <?php echo date('d/m/Y H:i:s'); ?></p>
+</main>
+
 
 <footer>
     <p>&copy; Copyright Matthis DoQuocBao | 42 Quebec</p>
@@ -33,4 +36,3 @@
 
 </html>
 
-//  <p>Date et Heure actuelle : <?php echo date('d/m/Y H:i:s'); ?></p>
--- www/webserv.com/cgi-pass/index.php
+++ www/webserv.com/cgi-pass/index.php
@@ -5,25 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webserv 42</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 <header>
     <h1><a href="../accueille.html">Werbserv 42</a></h1>
+    <div class="test">
+        <a href="../html/index.html" class="static out">Static request</a>
+        <a href="index.php" class="cgi in">Dynamic request</a>
+    </div>
+    <aside>
+        <a href="get.php" class="cgi request out">GET request</a>
+        <a href="post.php" class="cgi request out">POST request</a>
+        <a href="delete.php" class="cgi request out">DELETE request</a>
+    </aside>
 </header>
 
-<div class="index">
-    <aside>
-        <a href="../accueille.html" class="aside">GET request</a>
-        <a href="../accueille.html" class="aside">POST request</a>
-        <a href="../accueille.html" class="aside">DELETE request</a>
-    </aside>
-    <main>
-        <img src="../image/static-site.png" alt="Static site" class="site">
-    </main>
-</div>
+<main>
+
+    <img src="../image/dynamic-site.png" alt="Dynamic site" class="site">
+</main>
+
 
 <footer>
     <p>&copy; Copyright Matthis DoQuocBao | 42 Quebec</p>
@@ -33,4 +37,4 @@
 
 </html>
 
-//  <p>Date et Heure actuelle : <?php echo date('d/m/Y H:i:s'); ?></p>
+<!--  <p>Date et Heure actuelle : --><?php //echo date('d/m/Y H:i:s'); ?><!--</p>-->
